58105423f4404bfb401be2b2c6bfaaab5208b45c
Patrick Koss <49844980+PatrickKoss@users.noreply.github.com>
2023-04-08 21:02:34 +0200

    Update lspconfig.lua

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,15 +3,6 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
---local servers = { "html", "cssls", "tsserver", "clangd", "rust-analyzer", "gopls", "python-lsp-server", "golangci-lint-langserver", "marksman", "sqls", "lua", "pyright", "pyre", "yaml-language-server", "glint", "docker-compose-language-service", "dockerfile-language-server", "cloure-lsp", "cmake-language-server", "golangci-lint-langserver", "lua-language-server", "terraform-ls", "tflint", "vim-language-server" }
-
---for _, lsp in ipairs(servers) do
---  lspconfig[lsp].setup {
---    on_attach = on_attach,
---    capabilities = capabilities,
---  }
---end
-
 local servers = {
   "html", "cssls", "tsserver", "clangd", "rust_analyzer", "gopls",
   "pyright", "yamlls", "dockerls", "clojure_lsp", "cmake",
