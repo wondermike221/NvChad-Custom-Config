da82ed88da60155a0468f5d2ed46eae21926226f
PatrickKoss <pati.koss@gmx.de>
2023-04-11 20:05:44 +0200

    add spellcheck

--- plugins/init.lua
+++ plugins/init.lua
@@ -7,6 +7,7 @@ vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+vim.o.spell = true
 
 ---@type {[PluginName]: NvPluginConfig|false}
 local plugins = {
