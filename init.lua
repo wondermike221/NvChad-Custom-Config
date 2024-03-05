71c98f1b0f224e8be2569c4717489519da2e4cee
Michael <michael.hixon@hotmail.com>
2024-03-05 11:00:19 -0700

    open with nvim-tree's root in directory instead of cwd

--- init.lua
+++ init.lua
@@ -1,5 +1,20 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function(data)
+      local directory = vim.fn.isdirectory(data.file) == 1
+  
+      if not directory then
+        return
+      end
+      
+      vim.cmd.cd(data.file)
+  
+      require("nvim-tree.api").tree.open()
+    end,
+})
+  
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
