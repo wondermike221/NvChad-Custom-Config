c1acdc9c26b70d7dfeb03a96cd8a7d276278fb82
Michael Hixon <michael.hixon@hotmail.com>
2024-04-21 01:18:30 -0600

    fix deno lsp

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -6,7 +6,7 @@ local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
 local servers = {
-  "html", "cssls", "tsserver", "clangd", "rust_analyzer", "gopls",
+  "html", "cssls", "clangd", "rust_analyzer", "gopls",
   "pyright", "yamlls", "dockerls", "clojure_lsp", "cmake",
   "terraformls", "vimls"
 }
@@ -17,5 +17,18 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig["denols"].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+lspconfig["tsserver"].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false,
+}
 
 return M
